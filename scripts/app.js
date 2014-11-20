eb60c95447946fd15629581bf0b7029feb6e4a1b
jpbatz <jph@hawaii.edu>
2014-11-19 21:23:15 -1000

    mba initial commit

---
+++ scripts/app.js
@@ -0,0 +1,20 @@
+$(function() {
+  // Write all your code here
+  function viewBlogPosts() {
+
+  }
+
+  function viewStudents() {
+
+  }
+
+  $("#my_link").on("click", function() {
+
+  });
+
+  $("#my_link").on("click", function() {
+
+  });
+
+
+});
