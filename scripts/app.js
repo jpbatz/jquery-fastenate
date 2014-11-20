98cdaef4153a981fc1e66dcee33dbcad30a6b76d
jpbatz <jph@hawaii.edu>
2014-11-19 21:30:54 -1000

    mba include ajax functions for blogs and students

--- scripts/app.js
+++ scripts/app.js
@@ -8,12 +8,42 @@ $(function() {
 
   }
 
-  $("#my_link").on("click", function() {
-
+  $("#nav_items #blogs").on("click", function() {
+    $.ajax({
+      type: "GET",
+      url: "https://api.mongolab.com/api/1/databases/devleague_jquery/collections/blog_posts",
+      contentType: "application/json",
+      data: {
+        // Query string data from the url goes here (the part after the question mark ? in a url)
+        "apiKey": "QMnw2qjgl4l0LDx3RbN2NRlMEu_vCipC"
+      },
+      success: function(response) {
+        console.log(response); // See what it prints out
+        // Call the appropriate function here, viewBlogPosts or viewStudents
+      },
+      error: function(request, errorType, errorMessage) {
+        console.log("ERROR: " + errorMessage);
+      }
+    });
   });
 
-  $("#my_link").on("click", function() {
-
+  $("#nav_items #students").on("click", function() {
+    $.ajax({
+      type: "GET",
+      url: "https://api.mongolab.com/api/1/databases/devleague_jquery/collections/students",
+      contentType: "application/json",
+      data: {
+        // Query string data from the url goes here (the part after the question mark ? in a url)
+        "apiKey": "QMnw2qjgl4l0LDx3RbN2NRlMEu_vCipC"
+      },
+      success: function(response) {
+        console.log(response); // See what it prints out
+        // Call the appropriate function here, viewBlogPosts or viewStudents
+      },
+      error: function(request, errorType, errorMessage) {
+        console.log("ERROR: " + errorMessage);
+      }
+    });
   });
 
 
